edcead0b7b4eaee4d57311ce4f3578eb9445b78e
Scaon <117754291+DAQG@users.noreply.github.com>
2022-11-22 11:50:53 -0500

    Add files via upload

---
+++ Cajero-electronico-ATM-/ATM.java
@@ -0,0 +1,27 @@
+public class ATM {
+    String titular;
+    double cantidad;
+
+    public ATM() {
+        String titular = "Daniel Quishpe";
+        double cantidad;
+    }
+
+    public String getTitular() {
+        return titular;
+    }
+
+    public void setTitular(String titular) {
+        this.titular = titular;
+    }
+
+    public double getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(double cantidad) {
+        this.cantidad = cantidad;
+    }
+}
+
+
